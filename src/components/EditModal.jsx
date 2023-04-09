79e7b88b35361831d5778361f22b388426e88bee
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-08 18:18:23 -0700

    update added

---
+++ src/components/EditModal.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+const EditModal = ({item, text, setText, initialState, sendEdit}) => {
+    
+
+    useEffect(() => {
+        setText(initialState)
+    }, [initialState])
+  return (
+    <div className='w-screen h-screen bg-gray-400 bg-opacity-80 absolute top-0 left-0 flex justify-center items-center'>
+        <input 
+            type="text"
+            className='py-2 px-4 rounded-tl-md rounded-bl-md'
+            value={text} 
+            onChange={(e) => setText(e.target.value)} 
+            />
+        <input 
+            type="submit"
+            className='bg-indigo-500 py-2 px-4 text-white rounded-tr-md rounded-br-md font-bold'
+            value="UPDATE" 
+            onClick={sendEdit} 
+            />
+    </div>
+  )
+}
+
+export default EditModal
--- src/components/NoteCollection.jsx
+++ src/components/NoteCollection.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import EditModal from './EditModal';
+import EditIcon from './EditIcon';
 
 const URL = "https://note-node-crud.onrender.com/api/notes"
 
 const NoteCollection = () => {
     const [notes, setNotes] = useState([])
+    const [selectedItem, setSelectedItem] = useState({})
+    const [showEdit, setShowEdit] = useState(false)
+    const [text, setText] = useState('')
+
     const fetchNotes = async () => {
         const response = await fetch(URL);
         const data = await response.json();
@@ -42,6 +48,31 @@ const NoteCollection = () => {
     useEffect(() => {
         fetchNotes()
     }, [notes])
+
+    const handleEdit = (item) => {
+      setShowEdit(true)
+      setSelectedItem(item)
+      setText(selectedItem.text)
+    }
+
+    const sendEdit = async(item) => {
+      try {
+        const response = await fetch(`${URL}/${item._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ text: text }),
+        });
+        const updatedNote = await response.json();
+        setNotes((prevNotes) =>
+          prevNotes.map((note) => (note._id === updatedNote._id ? updatedNote : note))
+        );
+      } catch (err) {
+        console.error(err.message);
+      }
+      setShowEdit(false)
+    }
   return (
     <>
     <h1 className='mt-6 text-xl font-bold text-gray-600'>All Notes</h1>
@@ -53,10 +84,16 @@ const NoteCollection = () => {
                         <input type="checkbox" checked={item.completed} />
                         <li className={`${item.completed ? 'line-through text-gray-400' : ''} cursor-pointer`}>{item.text}</li>
                     </div>
-                    <p onClick={() => handleDelete(item)} className='font-bold cursor-pointer hover:text-gray-400 duration-300 text-xl'>X</p>
+                    <div className='flex gap-x-4 items-center'>
+                      <EditIcon 
+                        handleClick={() => handleEdit(item)}  
+                      />
+                      <p onClick={() => handleDelete(item)} className='font-bold cursor-pointer hover:text-gray-400 duration-300 text-xl'>X</p>
+                    </div>
                 </div>
             )
-        })}
+          })}
+          {showEdit ? <EditModal item={selectedItem} initialState={selectedItem.text} sendEdit={() => sendEdit(selectedItem)} text={text} setText={setText} /> : ''}
     </ul>
     </>
   )
