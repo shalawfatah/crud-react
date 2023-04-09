476fb20f06c1add45c74b3cd64c4fe9b1c0c2ab3
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-09 16:33:40 -0700

    button

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -15,7 +15,7 @@ const EditModal = ({item, text, setText, initialState, sendEdit}) => {
             />
         <input 
             type="submit"
-            className='bg-indigo-500 py-2 px-4 text-white rounded-tr-md rounded-br-md font-bold cursor-pointer'
+            className='bg-indigo-500 py-2 px-4 text-white rounded-tr-md rounded-br-md font-bold'
             value="UPDATE" 
             onClick={sendEdit} 
             />
